57d57e38a6e00b199e2e12ae63655ca8925f6031
abubakr587 <faxriddinovabubakr36@gmail.com>
2025-05-05 10:21:39 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+alert ("Salom men sizga ozim haqimda aytib bermoqchima")
+
+
+let ism = prompt('ism kiriting...');
+console.log('ism: ' , ism);
+
+let yosh = prompt('yoshingizni kiriting...');
+console.log('yoshim: ' , yosh);
+
+let fan = prompt('yaxshi korgan faningizni kiriting')
+console.log('fanim: ' , fan);
+
+let manzil = prompt('manzilingizni kiriting...');
+console.log('manzilim ' , manzil);
+
+
+
+alert (" Salom mening ismim " + ism + " va men " + yosh + " yosh daman "   + fan + " meni yaxshi korgan fanim men " + manzil + " danman " )
